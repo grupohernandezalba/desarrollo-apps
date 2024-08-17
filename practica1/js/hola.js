a3e0c700e1792472fbf2c594061cd4c48a43dca4
Marco Antonio Hernandez Alba <marco@grupohernandezalba.com>
2024-08-16 21:57:05 -0600

    tsconfig.json

--- practica1/js/hola.js
+++ practica1/js/hola.js
@@ -1,3 +1,9 @@
-var mensaje = 'Hola Mundo!';
+"use strict";
+let mensaje = 'Hola Mundo!';
 mensaje = 'Hola TS!';
 console.log(mensaje);
+let numero = 10;
+console.log(numero);
+// Comandos para compilar TS: tsc hola.ts
+// tsc --init (crea el archivo tsconfig.json) 
+// tsc -w (compila y ejecuta el archivo ts)
--- practica1/src/hola.ts
+++ practica1/src/hola.ts
@@ -3,3 +3,10 @@ let mensaje:string = 'Hola Mundo!';
 mensaje = 'Hola TS!';
 
 console.log(mensaje);
+
+let numero:number = 10;
+console.log(numero);
+
+// Comandos para compilar TS: tsc hola.ts
+// tsc --init (crea el archivo tsconfig.json) 
+// tsc -w (compila y ejecuta el archivo ts)
