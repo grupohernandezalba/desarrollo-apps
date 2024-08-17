d7c6d829047c62bab4e7ba70d11242cad131653b
Marco Antonio Hernandez Alba <marco@grupohernandezalba.com>
2024-08-16 22:03:19 -0600

    versiones de js

--- practica1/src/hola.ts
+++ practica1/src/hola.ts
@@ -10,3 +10,11 @@ console.log(numero);
 // Comandos para compilar TS: tsc hola.ts
 // tsc --init (crea el archivo tsconfig.json) 
 // tsc -w (compila y ejecuta el archivo ts)
+
+
+// Versiones de JS
+// https://www.w3schools.com/js/js_versions.asp
+
+
+// CommonJS
+//https://stackoverflow.com/questions/41326485/difference-between-module-type-in-tsconfig-json
