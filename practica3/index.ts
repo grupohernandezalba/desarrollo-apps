58d920916777a7de1434ad1a06bc94e4149c3f0c
Marco Antonio Hernandez Alba <marco@grupohernandezalba.com>
2024-08-26 21:12:19 -0600

    tuplas

--- practica3/index.ts
+++ practica3/index.ts
@@ -18,6 +18,7 @@ console.log(numeros2);
 
 let miArray: number[] = [1,2,3,4,5];
 let miArray2: Array<number> = [1,2,3,4,5];
+//Union de tipos 
 let miArray3: (number | string)[] = [1,2,3,4,5,'hola'];
 let miArray4: Array<number | string> = [1,2,3,4,5,'hola'];
 let miArray5: Array<any> = [1,2,3,4,5,'hola',true];
@@ -44,9 +45,44 @@ console.log('Array:',miArray8);
  
 //Tuplas
 
-let tupla:[string, number] = ["Jorge", 35];
-console.log(tupla);
-console.log(typeof tupla);
+    let tupla:[string, number] = ["Jorge", 35];
+    console.log(tupla);
+    console.log(typeof tupla);
+    console.log(tupla[0]);
+
+
+    // define our tuple
+    let ourTuple: [number, boolean, string];
+
+    // initialize correctly
+    //ourTuple = [5, false, 'Coding God was here',20];
+    //ourTuple = ['Hola', false, 'Coding God was here'];
+
+    ourTuple = [5, false, 'Coding God was here'];
+
+    console.log(ourTuple);
+    //console.log(ourTuple[3]);
+
+    // define our readonly tuple
+    const ourReadonlyTuple: readonly [number, boolean, string] = [5, true, 'The Real Coding God'];
+    // throws error as it is readonly.
+    //ourReadonlyTuple.push('Coding God took a day off');
+
+    console.log(ourReadonlyTuple);
+
+    //ourReadonlyTuple[0] = 10;
+    //ourReadonlyTuple[1] = false;
+
+    //Named Tuple
+    const graph: [x: number, y: number] = [55.2, 41.3];
+    console.log(graph);
+
+    // Desestructuración de tuplas
+    let tupla2:[string, number] = ["Jorge", 35];
+    let [nombre2, edad2] = tupla2;
+    console.log(nombre2);
+    console.log(edad2);
+
 
 
 //Enum
@@ -62,6 +98,57 @@ console.log(c);
 enum Color2 {Rojo = 1, Verde, Azul};
 let c2:Color2 = Color2.Rojo;
 console.log(c2);
+
+
+    enum CardinalDirections {
+        North,
+        East,
+        South,
+        West
+    }
+    let currentDirection = CardinalDirections.North;
+    // logs 0
+    console.log(currentDirection);
+    console.log(CardinalDirections.North);
+    console.log(CardinalDirections.West);
+
+
+    enum StatusCodes {
+        NotFound = 404,
+        Success = 200,
+        Accepted = 202,
+        BadRequest = 400
+      }
+      // logs 404
+      console.log(StatusCodes.NotFound);
+      // logs 200
+      console.log(StatusCodes.Success);
+
+
+      enum CardinalDirections2 {
+        North = 'North',
+        East = "East",
+        South = "South",
+        West = "West"
+      };
+      // logs "North"
+      console.log(CardinalDirections2.North);
+      // logs "West"
+      console.log(CardinalDirections2.West);
+
+
+
+
+enum Volumen {
+    min = 1,
+    medio = 5,
+    max = 10
+}   
+
+let audio:number = Volumen.medio;
+
+console.log(audio);
+
 
 
 //Cast
@@ -102,18 +189,6 @@ hero[2] = false;
 
 console.log(hero);
 
-
-//Enum
-
-enum Volumen {
-    min = 1,
-    medio = 5,
-    max = 10
-}   
-
-let audio:number = Volumen.medio;
-
-console.log(audio);
 
 
 //void
