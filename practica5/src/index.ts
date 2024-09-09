1de5dfb50ec0fac91ce48cf715907ccd4bf149a9
Marco Antonio Hernandez Alba <marco@grupohernandezalba.com>
2024-09-08 21:53:04 -0600

    practica8

--- practica5/src/index.ts
+++ practica5/src/index.ts
@@ -1,13 +1,4 @@
-// Create an Object
-const person = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 50
-  };
-  
-  // Destructuring
-  let {firstName, lastName} = person;
-  console.log(firstName,' ',lastName);
+
 
   // Create an Object
 const person2 = {
