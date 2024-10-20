478ee5df6a4e4de8a3a930b0121960fd19fe5347
Marco Antonio Hernandez Alba <marco@grupohernandezalba.com>
2024-10-20 13:31:14 -0600

    practica23-axios

--- practica19-ejs/index.js
+++ practica19-ejs/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
 
 //View engine es un motor de plantillas que nos permite renderizar vistas en el servidor
 // Usaremos EJS (Embedded JavaScript) para renderizar vistas en el servidor
@@ -9,6 +10,7 @@ const app = express();
 // app.set('view engine', 'ejs');
 
 app.set('view engine', 'ejs');
+app.engine('ejs', require('ejs').__express);
 
 app.listen(3000, () => {
     console.log('Servidor iniciado en http://localhost:3000');
@@ -34,19 +36,20 @@ app.get('/', (req, res) => {
         { titulo: 'Artículo 3', autor: 'Juan Perez', contenido: 'Lorem ipsum dolor sit amet consectetur' },
     ];
 
-    res.render('index', { titulo: 'Inicio', blogs : blogs});
+    res.render( __dirname + '/views/index', { titulo: 'Inicio', blogs : blogs});
+    //res.render('index', { titulo: 'Inicio', blogs : blogs});
 });
 
 app.get('/nosotros', (req, res) => {
-    res.render('nosotros', { titulo: 'Nosotros' });
+    res.render( __dirname + '/views/nosotros', { titulo: 'Nosotros' });
 });
 
 app.get('/agregar', (req, res) => {
-    res.render('agregar', { titulo: 'Agregar blog' });
+    res.render(__dirname + '/views/agregar', { titulo: 'Agregar blog' });
 });
 
 app.get('/contacto', (req, res) => {
-    res.redirect('/nosotros');
+    res.redirect(__dirname + '/views/nosotros');
 });
 
 app.use((req, res) => {
@@ -55,4 +58,11 @@ app.use((req, res) => {
 
 
 
-
+// https://its-amit.medium.com/how-to-make-build-for-express-js-node-js-using-webpack-and-deployment-on-docker-9cd219ba24a2
+// Para hacer el deploy a producción, ejecutamos el siguiente comando en la terminal:
+// npm run build
+// Esto generará un archivo final.js en la carpeta dist
+// Luego, ejecutamos el siguiente comando en la terminal:
+// node dist/final.js
+// Esto iniciará el servidor en modo producción
+// Para detener el servidor, presionamos Ctrl + C en la terminal
---
+++ practica23-axios/routes/articulosRoutes.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+
+//https://www.convertsimple.com/convert-javascript-to-json/
+
+//Validate JSON
+//https://jsonlint.com/
+//https://jsonformatter.curiousconcept.com/
+
+// Fetch
+//https://www.w3schools.com/jsref/api_fetch.asp
+//https://www.freecodecamp.org/espanol/news/tutorial-de-fetch-api-en-javascript-con-ejemplos-de-js-fetch-post-y-header/
+
+
+
+const articulos = [];
+
+const obtieneArticulos = () => {
+
+
+    axios.get('https://grupohernandezalba.com/articulos.json')
+    .then(response => {
+        console.log(response.data.articulos);
+        response.data.articulos.forEach(articulo => {
+            articulos.push(articulo);
+        });
+        console.log('Obteniendo articulos:',articulos);
+    })
+    .catch(err => console.log('Solicitud fallida', err));
+
+};
+
+router.get('/articulos', (req, res) => {
+    obtieneArticulos();
+    res.render('articulos', { titulo: 'Artículos', articulos : articulos });
+});
+
+router.get('/articulos/nuevo', (req, res) => {
+    res.render('nuevo', { titulo: 'Nuevo artículo' });
+});
+
+router.get('/articulos/:id', (req, res) => {
+    console.log(articulos[req.params.id]);
+    res.render('detalleArticulo', { titulo: 'Detalle artículo', articulo: articulos[req.params.id] });
+    console.log(req.params.id);
+}); 
+
+
+
+
+module.exports = router;
